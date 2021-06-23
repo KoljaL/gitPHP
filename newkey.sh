983b6fa547abd76d04700365f65f2bc6901527a0
KoljaL <lasar@rasal.de>
2021-06-23 08:42:00 +0200

    remove newkey.sh

--- newkey.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-expect -c "spawn ssh-keygen -b 2048 -t rsa -f /tmp/sshkey -q
-expect \"Enter passphrase (empty for no passphrase):\"
-send \"\r\"
-expect \"Enter same passphrase again:\"
-send \"\r\"
-"
