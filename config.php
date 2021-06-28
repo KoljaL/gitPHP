666b637a6225a3f938ae43b8ffab46b4677979c8
KoljaL <lasar@rasal.de>
2021-06-29 00:33:37 +0200

    input rewrite

--- config.php
+++ config.php
@@ -13,6 +13,7 @@ $resp = [
     ),
     'presets' => array(
         'startpage' => 'start', 
+        'create new repo' => array('init', 'add', 'branch', 'commit', 'create_repo', 'push'),
         'commit and push' => array('add', 'commit', 'push'),
         'info and stats' => array('remote_origin_url', 'remote_show_origin', 'last_commit', 'all_commits'),
         'all' => 'all',
@@ -42,6 +43,24 @@ $resp = [
             'text' => 'This command updates the index using the current content found in the working tree, to prepare the content staged for the next commit.'
         ),
 
+        'init' => array(
+            'command' => 'git init',
+            'title' => 'Create an empty Git repository',  
+            'button' => 'init',
+            'tooltip' => 'git init',
+            'infolink' => 'https://git-scm.com/docs/git-init',
+            'text' => 'This command creates an empty Git repository, or reinitialize an existing one, - basically a .git directory with subdirectories for objects, refs/heads, refs/tags, and template files. An initial branch without any commits will be created.'
+        ),
+
+        // 'new_branch' => array(
+        //     'command' => 'git checkout -b BRANCHNAME',
+        //     'title' => 'Create a new branch in your local repository',  
+        //     'button' => 'new branch',
+        //     'tooltip' => 'git init',
+        //     'infolink' => 'https://git-scm.com/docs/git-branch',
+        //     'text' => 'This command createsa new branch in your local repository. Note that this will create the new branch, but it will not switch the working tree to it; use "git switch <newbranch>" to switch to the new branch.'
+        // ),
+
         'commit' => array(
             'command' => 'git commit -m "new commit" ',
             'title' => 'Commit all changes to the local repository',  
@@ -54,6 +73,34 @@ $resp = [
                 'placeholder' => '',
                 'value' => 'new commit',
 
+            ),
+        ),
+
+        'branch' => array(
+            'command' => 'git branch -M main ',
+            'title' => 'Create a new branch',  
+            'button' => 'branch',
+            'tooltip' => 'git branch -M main',
+            'infolink' => 'https://git-scm.com/docs/git-branch',
+            'text' => 'This command creates a new branch named \'main\' which points to the current HEAD. ',
+            'formfield' => array(
+                'textfield' => 'textfield_new_branch',
+                'placeholder' => '',
+                'value' => 'main',
+            ),
+        ),
+
+        'create_repo' => array(
+            'command' => 'gh repo create REPONAME -y -d "description" --private ',
+            'title' => 'Create a new GitHub repository',  
+            'button' => 'new repo',
+            'tooltip' => 'gh repo create REPONAME -y -d "description" --private',
+            'infolink' => 'https://cli.github.com/manual/gh_repo_create',
+            'text' => 'When the current directory is a local git repository, the new repository will be added as the "origin" git remote. Otherwise, the command will prompt to clone the new repository into a sub-directory.',
+            'formfield' => array(
+                'textfield' => 'textfield_repo_description',
+                'placeholder' => '',
+                'value' => 'description',
             ),
         ),
 
@@ -113,3 +160,26 @@ $resp = [
 
 ];
 
+// search for a repo: https://api.github.com/repos/<user>/<repo>
+
+// create new repo: 
+
+
+// git init
+// git add .
+// git commit -m "tt"
+// git branch -M main
+// gh repo create AHA -y -d "ne beschreibung" --private
+// git push -u origin main
+
+
+// delete a repo
+//   # one time:
+// $ gh alias set delete 'api -X DELETE repos/$1'
+// $ gh auth refresh -h github.com -s delete_repo
+// # usage (WARNING: no confirmation!)
+// gh delete user/myrepo
+
+// or this
+//git push origin --delete main
+// https://stackoverflow.com/questions/2003505/how-do-i-delete-a-git-branch-locally-and-remotely
