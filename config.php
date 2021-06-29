4bda2bce5d3f7cb112d729c353c527897f6cc5b2
KoljaL <lasar@rasal.de>
2021-06-29 09:59:21 +0200

    new commit

--- config.php
+++ config.php
@@ -1,6 +1,4 @@
 <?php
-
-
 $resp = [
     'user' => array(
         'test' => array(
@@ -8,9 +6,6 @@ $resp = [
         ),
     ),
     'session_time' => 300,
-    'CustomCommandList' => array(
-        'git config --get remote.origin.url',' git remote show origin'
-    ),
     'presets' => array(
         'startpage' => 'start', 
         'create new repo' => array('init', 'add', 'branch', 'commit', 'create_repo', 'push'),
@@ -21,33 +16,17 @@ $resp = [
     // to avoit WARNINGs every entry has to have every key set
     'commands' => array(
 
-        'custom_command' => array(
-            'command' =>'/',
-            'title' => 'Custom Command',  
-            'button' => 'Custom Command',
-            'tooltip' => 'send what you want',
-            'infolink' => '',
-            'text' => '',
-            'formfield' => array(
-                'datalist' => 'datalist_customcommand',
-                'placeholder' => 'Choose or type'
-            )
-        ),
-
         'commit_and_push' => array(
             'command' => 'git add . && git commit -m "new commit" && git push origin input_rewrite ',
             'title' => 'Add, commit & push in onecommand',  
             'button' => 'add & push',
-            'tooltip' => 'git add .',
-            'infolink' => '',
-            'text' => 'Mention the commit message and the origin branch name (main)'
+            'text' => 'Mind the commit message and the origin branch name (main)'
         ),
 
         'add' => array(
             'command' => 'git add .',
             'title' => 'Add all files to git index',  
             'button' => 'add',
-            'tooltip' => 'git add .',
             'infolink' => 'https://git-scm.com/docs/git-add',
             'text' => 'This command updates the index using the current content found in the working tree, to prepare the content staged for the next commit.'
         ),
@@ -56,68 +35,38 @@ $resp = [
             'command' => 'git init',
             'title' => 'Create an empty Git repository',  
             'button' => 'init',
-            'tooltip' => 'git init',
             'infolink' => 'https://git-scm.com/docs/git-init',
             'text' => 'This command creates an empty Git repository, or reinitialize an existing one, - basically a .git directory with subdirectories for objects, refs/heads, refs/tags, and template files. An initial branch without any commits will be created.'
         ),
-
-        // 'new_branch' => array(
-        //     'command' => 'git checkout -b BRANCHNAME',
-        //     'title' => 'Create a new branch in your local repository',  
-        //     'button' => 'new branch',
-        //     'tooltip' => 'git init',
-        //     'infolink' => 'https://git-scm.com/docs/git-branch',
-        //     'text' => 'This command createsa new branch in your local repository. Note that this will create the new branch, but it will not switch the working tree to it; use "git switch <newbranch>" to switch to the new branch.'
-        // ),
 
         'commit' => array(
             'command' => 'git commit -m "new commit" ',
             'title' => 'Commit all changes to the local repository',  
             'button' => 'commit',
-            'tooltip' => 'git commit -m "new commit"',
             'infolink' => 'https://git-scm.com/docs/git-commit',
             'text' => 'Create a new commit containing the current contents of the index and the given log message describing the changes. ',
-            'formfield' => array(
-                'textfield' => 'textfield_commit',
-                'placeholder' => '',
-                'value' => 'new commit',
-
-            ),
         ),
 
         'branch' => array(
             'command' => 'git branch -M main ',
             'title' => 'Create a new branch',  
             'button' => 'branch',
-            'tooltip' => 'git branch -M main',
             'infolink' => 'https://git-scm.com/docs/git-branch',
             'text' => 'This command creates a new branch named \'main\' which points to the current HEAD. ',
-            'formfield' => array(
-                'textfield' => 'textfield_new_branch',
-                'placeholder' => '',
-                'value' => 'main',
-            ),
         ),
 
         'create_repo' => array(
             'command' => 'gh repo create REPONAME -y -d "description" --private ',
             'title' => 'Create a new GitHub repository',  
             'button' => 'new repo',
-            'tooltip' => 'gh repo create REPONAME -y -d "description" --private',
             'infolink' => 'https://cli.github.com/manual/gh_repo_create',
             'text' => 'When the current directory is a local git repository, the new repository will be added as the "origin" git remote. Otherwise, the command will prompt to clone the new repository into a sub-directory.',
-            'formfield' => array(
-                'textfield' => 'textfield_repo_description',
-                'placeholder' => '',
-                'value' => 'description',
-            ),
         ),
 
         'push' => array(
             'command' => 'git push origin main ',
             'title' => 'Push the local repository to the the remote main branch',  
             'button' => 'push',
-            'tooltip' => 'git push origin main',
             'infolink' => 'https://git-scm.com/docs/git-push',
             'text' => 'Updates remote refs using local refs, while sending objects necessary to complete the given refs. '
         ),
@@ -126,7 +75,6 @@ $resp = [
             'command' => 'git config --get remote.origin.url ',
             'title' => 'Show remote origin URL',  
             'button' => 'origin URL',
-            'tooltip' => 'git config --get remote.origin.url',
             'infolink' => '',
             'text' => 'Show origin URL'
         ),
@@ -135,7 +83,6 @@ $resp = [
             'command' => 'git remote show origin ',
             'title' => 'Show remote origin ata',  
             'button' => 'origin data',
-            'tooltip' => 'git remote show origin',
             'infolink' => '',
             'text' => 'Show remote origin data'
         ),
@@ -144,18 +91,16 @@ $resp = [
             'command' => 'git log --stat -1',
             'title' => 'Last commit',  
             'button' => 'last commit',
-            'tooltip' => 'git log --stat -1',
             'infolink' => '',
             'text' => 'Last commit'
         ),
 
         'all_commits' => array(
-            'command' => 'git log --pretty=format:"%h - %an, %ar : %s"',
-            'title' => 'All commits',  
+            'command' => 'git log --pretty=format:"%h - %an, %ar : %s" --reverse',
+            'title' => 'Show all commits',  
             'button' => 'all commits',
-            'tooltip' => 'git log --pretty=format:"%h - %an, %ar : %s"',
             'infolink' => '',
-            'text' => 'All commits'
+            'text' => 'Show all commits'
         ),
 
     ),
